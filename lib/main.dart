7b2a7ebc9bf2f59f03cacb915de24a6297d81c02
= <=>
2021-07-02 15:07:40 +0700

    18 Card Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-//12 STACK DAN ALIGN
-
-// STACK
-// kalo di photoshop digunakan untuk layer 
-// layer 1 = background 
-// layer 2 = text anggota kelas 
-// layer 3 = button 
-
-//ALIGN
-//biasalah ky kuliah
+//18 Card Widget
+//pakai extract method (CardTracking) untuk perulangannya 
+//
+//
+//plis gausah dikomen iki aplikasi opo, iki sokor sokor ae ngawur slicing aplikasi ga jelas gado gado ye
 
 void main() {
   runApp(MyApp());
@@ -22,61 +18,61 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Latihan Stack'),
-          ),
-          body: Stack(children: <Widget>[
-            Container(
-              color: Colors.blueGrey,
-            ),
-            Padding(
-              padding: EdgeInsets.all(19),
-              child: ListView(children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Column(
-                      children: [
-                        Text(
-                          "CURHAT DONG NGAB",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 30,
-                          ),
-                        ),
-                        Text(
-                          "MI2C",
-                          style: TextStyle(color: Colors.white, fontSize: 20),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 50, bottom: 20),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        " Ajeng Eka Y \n Ardhanika MRH \n Ayu Ariesta \n Brian Andri \n Destiara \n Dewi Okta \n Dika Ayu \n Dinda Jinggan \n Dyah Ayu \n \n Elsa Aprilia \n Erni Sri \n Falya Charismtul \n Ferika Asmara \n \n --JAYA MAHENDRA-- \n \n Lilik Nurwati \n Abdush Shidqi \n Luthfi R \n Masykur W \n NIo Dyah \n Osy Krisdayanti \n\n Rasyida K \n Syalwa N \n Tia W \n Yayang Nirmala N S \n Yuni R \n Zamrony",
-                        style: TextStyle(fontSize: 19, color: Colors.white),
-                      )
-                    ],
-                  ),
-                ),
-              ]),
-            ),
-            Align(
-
-              // custom
-              // x = vertikal y = horizontal 
-              // dimulai dari -1 0 1 . coba ae ngab custom sendiri
-              alignment: Alignment(0, 0.9),
-              // alignment: Alignment.bottomCenter,
-                child:
-                    RaisedButton(onPressed: () {}, child: Text("My Button"))),
-          ])),
+          body: ListView(children: [
+        Container(
+            padding: EdgeInsets.only(top: 50, left: 20),
+            height: 150,
+            width: 800,
+            color: Colors.green,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text(
+                "Friday, July 2",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              Text(
+                "Your Parcels",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              )
+            ])),
+        Container(
+          padding: EdgeInsets.only(top: 40, left: 20), 
+          child: 
+            Column( crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              Text(
+                "Tracking",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+            CardTracking(Icons.ac_unit,"AC"),
+            CardTracking(Icons.lock_clock,"JAM"),
+            CardTracking(Icons.access_alarm,"ALARAM"),
+            CardTracking(Icons.accessibility_new_outlined,"MAN"),
+            CardTracking(Icons.backpack,"BACKPACK"),
+            CardTracking(Icons.qr_code_rounded,"QR CODE"),
+            CardTracking(Icons.gamepad,"GAME"),
+            CardTracking(Icons.adb_sharp,"ANDROID"),
+          ],
+        )
+      )])),
     );
+  }
+
+  Card CardTracking(IconData iconData, String data) {
+    return Card(
+            child: Row(
+              children: [
+                Container(
+                    margin: EdgeInsets.all(15), child: Icon(iconData, color: Colors.green)),
+                Text(data),
+              ],
+            ),
+          );
   }
 }
